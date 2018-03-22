3dde55c9ed836f669532c1c69dd47ae43b313f8c
neifirst <e.mcculloch.arch@gmail.com>
2018-03-22 18:03:48 +0000

    added product class, properties, getters, and setters

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -1,4 +1,57 @@
 package models;
 
 public class Product {
+
+    private int id;
+    private String name;
+    private int price;
+    private int vat;
+    private SubCategory subCategory;
+
+
+    public Product(String name, int price, SubCategory subCategory) {
+        this.name = name;
+        this.price = price;
+        this.subCategory = subCategory;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getVat() {
+        return vat;
+    }
+
+    public void setVat(int vat) {
+        this.vat = vat;
+    }
+
+    public SubCategory getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(SubCategory subCategory) {
+        this.subCategory = subCategory;
+    }
 }
